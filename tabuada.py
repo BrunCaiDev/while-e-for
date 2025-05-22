64bb1c871ee840f572c06458466b610219513e04
BrunCaiDev <amebamaior@gmail.com>
2025-05-22 15:13:22 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,42 @@
+# numero = 5
+
+# for i in range(1, 11):
+#     print(f"{i} x {numero} = {i * numero}")
+
+
+# numero = 5
+# i = 1
+
+# while i <= 10:
+#     print(f" {i} x {numero} = {i * numero}")
+#     i += 1
+
+
+# Numero = int(input("Qual número você quer começar?"))
+# multiplicador = int(input("Por qual número você quer multiplicar?"))
+# final = int(input("Até qual número você vai multiplicar?"))
+
+# print("\n--------------------------------------------------")
+
+# while Numero <= final:
+#     print(f" {Numero} x {multiplicador} = {multiplicador * Numero}")
+#     Numero += 1 
+
+
+
+    
+Numero = int(input("Qual número você quer começar?"))
+multiplicador = int(input("Por qual número você quer multiplicar?"))
+final = int(input("Até qual número você vai multiplicar?"))
+
+print("\n--------------------------------------------------")
+
+for Numero in range(Numero, final + 1):
+    print(f" {Numero} x {multiplicador} = {multiplicador * Numero}")
+
+
+
+
+
+    
+
